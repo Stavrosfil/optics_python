383293575ff623ddbd1db193ead06734220aebe5
Stavros Filosidis <stavrosfil@gmail.com>
2019-08-22 00:35:51 +0300

    Added optic catalogs and installation script.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python3 -m rayopt.library $PWD/catalog/zemax/Glasscat
+python3 -m rayopt.library $PWD/catalog/zemax/Stockcat
+python3 -m rayopt.library $PWD/catalog/oslo/glc
+python3 -m rayopt.library $PWD/catalog/oslo/lmo
